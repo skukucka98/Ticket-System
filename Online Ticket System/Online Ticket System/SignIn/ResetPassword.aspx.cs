87e6e256d23a3181e245843f12eca3a0fcaf58fd
quandong2015 <henrydong90@gmail.com>
2015-04-22 18:57:56 -0400

    modified reset password

--- Online Ticket System/Online Ticket System/SignIn/ResetPassword.aspx.cs
+++ Online Ticket System/Online Ticket System/SignIn/ResetPassword.aspx.cs
@@ -15,13 +15,30 @@ namespace Online_Ticket_System.SignIn
         string id2;
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Request.QueryString["id1"] == null || Request.QueryString["id2"] == null)
+            {
+                Response.Redirect("../default.aspx");
+            }
+            else
+            {
                 string url = Request.RawUrl.ToString();
                 id1 = url.Substring(url.IndexOf('?'), url.IndexOf('&') - url.IndexOf('?'));
                 id1 = id1.Substring(5, id1.Length - 5);
                 id2 = url.Substring(url.IndexOf('&'), url.Length - (url.IndexOf('&')));
                 id2 = id2.Substring(5, id2.Length - 5);
-                if (Request.QueryString["id1"] == null || Request.QueryString["id2"] == null || Connection.GetUserbyUsernameandPassword(Connection.Decrypt(id1), Connection.Decrypt(id2)).Count == 0)
+                if (!IsPostBack)
+                {
+                    Session["id1"] = id1;
+                    Session["id2"] = id2;
+                }
+                else
+                {
+                    id1 = Session["id1"].ToString();
+                    id2 = Session["id2"].ToString();
+                }
+                if (Connection.GetUserbyUsernameandPassword(Connection.Decrypt(id1), Connection.Decrypt(id2)).Count == 0)
                     Response.Redirect("../default.aspx");
+            }
         }
 
         protected void RePaSubmit_Click(object sender, EventArgs e)
@@ -43,7 +60,7 @@ namespace Online_Ticket_System.SignIn
                 Connection.UpdatePassbyUnameEmail(Connection.Decrypt(id1), Connection.Decrypt(id2), RePass.Text);
                 RePaMessage.Text = "Successfully reset your password";
                 HttpCookie cookie = new HttpCookie("UserInfo");
-                ArrayList Userlist = Connection.GetUserbyUsernameandPassword(Connection.Decrypt(Request.QueryString["id1"]), RePass.Text);
+                ArrayList Userlist = Connection.GetUserbyUsernameandPassword(Connection.Decrypt(id1), RePass.Text);
                 foreach (Users user in Userlist)
                 {
                     cookie["id"] = user.ID.ToString();
@@ -61,12 +78,16 @@ namespace Online_Ticket_System.SignIn
                     cookie["Zip"] = user.Zipcode;
                 }
                 Response.Cookies.Add(cookie);
-                Response.Redirect("~/Default.aspx");                
+                Session["id1"] = null;
+                Session["id2"] = null;
+                Response.Redirect("~/Default.aspx");
             }
         }
 
         protected void RePaCancel_Click(object sender, EventArgs e)
         {
+            Session["id1"] = null;
+            Session["id2"] = null;
             Response.Redirect("~/Default.aspx");
         }
     }
